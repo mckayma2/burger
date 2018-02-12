fc889311f6c7fc2d019a6854847fee9da0d47e33
Mackay <mario.mackay@marineharvest.com>
2018-02-12 16:05:25 -0500

    upload

---
+++ public/assets/js/script.js
@@ -0,0 +1,17 @@
+console.log('javascript ready');
+
+var data = {
+			siblings:[
+					{name:'mario', age: 43}, 
+					{name:'camille', age: 47}
+				]
+			};
+console.log(data);
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "jquery ready!" );
+
+
+
+});
--- server.js
+++ server.js
@@ -1,2 +1,33 @@
+var port = process.env.PORT || 8080;
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
+var handlebars = require('handlebars');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.get('/', function (req, res) {
+ res.sendFile(path.join(__dirname + '/public/index.html'));
+	
+});
+
+app.get('/script', function (req, res) {
+ res.sendFile(path.join(__dirname + '/public/assets/js/script.js'));
+	
+});
+app.get('/hb', function (req, res) {
+ res.sendFile(path.join(__dirname + '/public/assets/js/handlebars-v4.0.11.js'));
+	
+});
+
+app.get('/jq', function (req, res) {
+ res.sendFile(path.join(__dirname + '/public/assets/js/jquery-3.3.1.min.js'));
+	
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
